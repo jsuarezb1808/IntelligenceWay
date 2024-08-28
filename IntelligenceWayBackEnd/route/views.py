1a776766695aa5335bd7f100e4debfa8b21b347c
Daniel Cruz <78978829+Daniale566@users.noreply.github.com>
2024-08-28 18:22:54 -0500

    Se agrega el primer login sin conexion externa

--- IntelligenceWayBackEnd/route/views.py
+++ IntelligenceWayBackEnd/route/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def profile(request):
+    user = request.user
+    auth_0user = user.social_auth.get(provider='auth0')
+    
+
+    user_data = {
+        'user_id': auth_0user.uid,
+        'name': user.first_name,
+        'picture': auth_0user.extra_data['picture']
+    }
+
+
+    context = {
+        'user_data' : json.dumps(user_data, indent=4),
+        'auth_0user' : auth_0user
+    }
+
+
+
+
+
+    return render(request, 'profile.html', context)
