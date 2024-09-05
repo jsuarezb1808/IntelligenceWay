bf8b97f9ea7c0a8ef1c53947eb6a1638d76ad04d
Axl <jsuarezb99@gmail.com>
2024-09-05 13:34:35 -0500

    added multimedia files

--- IntelligenceWayBackEnd/route/urls.py
+++ IntelligenceWayBackEnd/route/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('profile/', views.profile, name='profile'),
     path('', include('social_django.urls')),
+    path('survey/', views.LearningMethods,name='survey'),
 ]
+
