05f822426d1737edb2d4b14e8b357ce2ef5c261d
cortegons <cortegons@eafit.edu.co>
2024-10-15 16:47:19 -0500

    creada nueva signal para crear favorito junto a la cuenta

--- IntelligenceWayBackEnd/user/signals.py
+++ IntelligenceWayBackEnd/user/signals.py
@@ -2,9 +2,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from preferences.models import ModeloAprendizajeUsuario
 from .models import User
+from route.models import Favorito
 
 @receiver(post_save, sender=User)
 def crear_modelo_automáticamente(sender, instance, created, **kwargs):
     if created:
         # Se crea un carrito asociado al usuario si el usuario ha sido creado
         ModeloAprendizajeUsuario.objects.create(usuario=instance)
+        Favorito.objects.create(usuario=instance)
