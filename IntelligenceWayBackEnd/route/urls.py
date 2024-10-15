ff47a735afe1097a3a88ae626f79efe9a89e9e5b
jsuarezb1808 <root@DESKTOP-53DR3O3>
2024-10-15 16:54:34 -0500

    eliminado el entorno virtual

--- IntelligenceWayBackEnd/route/urls.py
+++ IntelligenceWayBackEnd/route/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    
+    path('create/', views.CreateRoute.as_view(), name="Crear_ruta"),
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
