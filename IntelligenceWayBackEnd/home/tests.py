3342f3ce2e4ae263506f7550d3987fd972f80f23
cortegons <cortegons@eafit.edu.co>
2024-10-15 22:09:07 -0500

    Actualizado el diseño de index, login, register, form, test

--- IntelligenceWayBackEnd/home/tests.py
+++ IntelligenceWayBackEnd/home/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class UrlTests(TestCase):
+
+    def test_home_url(self):
+        response = self.client.get(reverse('index'))  
+        self.assertEqual(response.status_code, 200)
