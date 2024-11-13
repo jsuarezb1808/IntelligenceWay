56c4ca03f8e5bbd82362f2e8be5702cd676de23a
Daniel Cruz <dacruzj@eafit.edu.co>
2024-11-13 11:48:52 -0500

    agregada página "About Us" con información sobre la plataforma y enlace a GitHub; actualizado menú de navegación

--- IntelligenceWayBackEnd/home/urls.py
+++ IntelligenceWayBackEnd/home/urls.py
@@ -3,4 +3,6 @@ from home import views
 
 urlpatterns = [
 	path('', views.IndexView.as_view(), name='index'),
+    path('about/', views.AboutUsView.as_view(), name='about_us'),
+
 ]
--- IntelligenceWayBackEnd/home/views.py
+++ IntelligenceWayBackEnd/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
+
 # Create your views here.
 
 class IndexView(View):
@@ -7,3 +9,6 @@ class IndexView(View):
     
     def get(self, request):
         return render(request, self.template_name)
+    
+class AboutUsView(TemplateView):
+    template_name = 'about_us.html'
