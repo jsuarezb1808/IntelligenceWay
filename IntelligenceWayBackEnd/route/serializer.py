97c2e2b5b7a4e49d4ef4e7573859504c3fcf107a
cortegons <cortegons@eafit.edu.co>
2024-11-16 19:00:16 -0500

    dockerfile

--- IntelligenceWayBackEnd/route/serializer.py
+++
@@ -1,9 +0,0 @@
-from .models import LearningMethods
-from rest_framework import serializers
-
-#se utiliza para convertir los datos a un Json
-class LearningMethodsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= LearningMethods
-        fields = '__all__'
-        
